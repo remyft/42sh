a20676ecda09beda0b8f1a04b81d86e86613035d
gilles BOURGEOIS <gbourgeo@student.42.fr>
2018-11-18 20:29:25 +0100

    add command.h structure and defines in token.h

--- include/token.h
+++ include/token.h
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 16:24:35 by gbourgeo          #+#    #+#             */
-/*   Updated: 2018/11/18 02:48:14 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2018/11/18 19:21:40 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 # define EXECUTE		(1 << 0)
 # define EXECUTE_IF_OK	(1 << 1)
 # define EXECUTE_IF_NOT	(1 << 2)
+
+# define WORDS			(1 << 0)
+# define OPERATOR		(1 << 1)
+# define QUOTED			(1 << 2)
+# define DQUOTED		(1 << 3)
+
+typedef struct      s_cmd
+{
+    char			type;
+    char			*cmd;
+	struct s_cmd	*left;
+	struct s_cmd	*right;
+}					t_cmd;
 
 typedef struct		s_var
 {
@@ -28,10 +41,12 @@ typedef struct		s_var
 typedef struct		s_token
 {
 	char			rights;
-	char			*command;
+	char			*line;
+	t_cmd			*head;
 	struct s_token	*next;
 }					t_token;
 
-t_token				*get_tokens(const char *buff);
+t_token				*get_tokens(char *buff);
+void				get_commands(t_token *tokens);
 
 #endif
