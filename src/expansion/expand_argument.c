4c0b68ef05ccf682b2718995ad3f70204f50995e
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-14 02:33:35 +0100

    [EXPANSION] removed quote_removal from expansions and add free_t_ret for better view on function operations

--- src/expansion/expand_argument.c
+++ src/expansion/expand_argument.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 22:38:14 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/07 22:40:05 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/13 23:19:47 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,15 @@ int				expand_argument(const char *buff, t_argument *arg, t_s_env *e)
 		return (0);
 	ft_memset(&param, 0, sizeof(param));
 	if (!(ret = ft_memalloc(sizeof(*ret))))
-		return (ERR_MALLOC);
+		return (expand_error(ERR_MALLOC, e->progname, ret));
 	param.e = e;
 	param.buff = buff + arg->token->head;
 	param.buff_len = arg->token->tail - arg->token->head;
 	param.expand = 1;
 	if ((error = expand_loop(ret, &param, token_end)) != ERR_NONE)
-		return (expand_error(error, e->progname, ret->word));
-// 	expand_pathname();
-	quote_removal(ret);
+		return (expand_error(error, e->progname, ret));
 	if (expand_end(ret, arg))
-		return (expand_error(ERR_MALLOC, e->progname, ret->word));
+		return (expand_error(ERR_MALLOC, e->progname, ret));
+	free_t_ret(&ret);
 	return (expand_argument(buff, arg->next, e));
 }
