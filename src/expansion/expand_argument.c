51c8f7429ad190cb1a019cbc88648f5503d5d2eb
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-14 22:14:50 +0100

    [EXPANSION] fieldsplit handler in expansions

--- src/expansion/expand_argument.c
+++ src/expansion/expand_argument.c
@@ -6,13 +6,24 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 22:38:14 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/13 23:19:47 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/14 19:16:09 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "expansion.h"
 #include "expansion_errors.h"
+
+static size_t	table_len(char **table)
+{
+	size_t		i;
+
+	i = 0;
+	if (table)
+		while (table[i])
+			i++;
+	return (i);
+}
 
 static int		token_end(t_exp *param)
 {
@@ -34,10 +45,14 @@ int				expand_argument(const char *buff, t_argument *arg, t_s_env *e)
 	param.buff = buff + arg->token->head;
 	param.buff_len = arg->token->tail - arg->token->head;
 	param.expand = 1;
+	param.fieldsplit = (arg->token->id != ASSIGNMENT_WORD);
 	if ((error = expand_loop(ret, &param, token_end)) != ERR_NONE)
 		return (expand_error(error, e->progname, ret));
 	if (expand_end(ret, arg))
 		return (expand_error(ERR_MALLOC, e->progname, ret));
+	if (arg->token && arg->token->prev && arg->token->prev->type == OPERATOR
+		&& table_len(arg->cmd) > 1)
+		return (expand_error(ERR_AMBIGUOUS, e->progname, ret));
 	free_t_ret(&ret);
 	return (expand_argument(buff, arg->next, e));
 }
