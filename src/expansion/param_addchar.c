33d6b9dd40d0ee295f56efa92f422551f17a5bf4
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-14 02:41:55 +0100

    [EXPANSION] fixed invalid read + remove bad check

--- src/expansion/param_addchar.c
+++ src/expansion/param_addchar.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/25 23:59:58 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/03 17:48:40 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/14 01:32:57 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,20 @@ static int	param_error(char *save, t_ret *ret)
 int			param_addchar(char c, t_ret *ret)
 {
 	char	*save;
+	size_t	i;
 
 	save = NULL;
+	i = 0;
 	if (ret->w_len == ret->w_max)
 	{
 		save = ret->word;
 		ret->w_max += RET_CHUNK;
-		if ((ret->word = ft_memalloc(ret->w_max)) == NULL)
+		if (!(ret->word = ft_memalloc(ret->w_max + 1)))
 			return (param_error(save, ret));
 	}
-	if (ret->word)
-	{
-		if (save)
-			ft_strcpy(ret->word, save);
-		ret->word[ret->w_len++] = c;
-	}
+	if (save)
+		ft_strcpy(ret->word, save);
+	ret->word[ret->w_len++] = c;
 	if (save)
 		free(save);
 	return (ERR_NONE);
