9e6b5da79318a6f385f795a73995b28cfbe6f817
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-19 23:35:17 +0100

    [EXPANSION] Bug on fieldspliting ( FIXED )

--- src/expansion/expand_dollar_parameter_value.c
+++ src/expansion/expand_dollar_parameter_value.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/27 04:42:31 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/16 23:09:22 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/19 22:52:39 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,6 @@ static int		expand_env(t_ret *subs, t_ret *para, t_exp *param)
 {
 	char		*word;
 
-	// if (!para->word)
-	// 	return (ERR_NONE);
 	word = &para->word[para->brace + para->hash + 1];
 	if (!(subs->word = exp_getnenv(word, param->e->public_env)))
 		subs->word = exp_getnenv(word, param->e->private_env);
