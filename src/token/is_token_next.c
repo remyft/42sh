e9442508db9ffa5677f289c1f2de651471177c52
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-22 08:14:04 +0100

    [ALL] Changed various files name to be more accurate

--- src/token/is_token_next.c
+++ src/token/is_token_next.c
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_token2.c                                        :+:      :+:    :+:   */
+/*   is_token_next.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 10:22:43 by gbourgeo          #+#    #+#             */
-/*   Updated: 2018/12/18 18:12:10 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/22 06:14:46 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "token.h"
+
+int			ft_isvalidname(t_param *param)
+{
+	const char	*str;
+	size_t		n;
+
+	str = param->buff + param->token->head;
+	n = param->i - param->token->head;
+	if (!n || ft_isdigit(*str))
+		return (0);
+	while (n--)
+		if (!ft_isalnum(*str) && *str != '_')
+			return (0);
+		else
+			str++;
+	return (1);
+}
 
 int			ft_isword(int c)
 {
