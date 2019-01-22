0cc9e909e2e88d8be180f20beeedc19f89e9e4cf
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-22 08:10:43 +0100

    [EXECUTION] Started handling redirections

--- src/execution/execute_list.c
+++ src/execution/execute_list.c
@@ -6,13 +6,14 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 02:19:16 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/20 22:49:12 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/22 07:46:49 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 #include "operator_types.h"
 #include "expansion.h"
+#include "redirection.h"
 
 static int	prepare_redirect(const char *buff, t_redirection *cmd, t_s_env *e)
 {
@@ -21,8 +22,8 @@ static int	prepare_redirect(const char *buff, t_redirection *cmd, t_s_env *e)
 	if (expand_argument(buff, cmd->arg, e))
 		return (1);
 	quote_removal(cmd->arg);
-	// if (!open_file(buff, cmd))
-	// 	return (1);
+	if (redirection(buff, cmd, e))
+		return (1);
 	return (prepare_redirect(buff, cmd->next, e));
 }
 
@@ -49,7 +50,7 @@ static int	execute_ao_list(const char *buff, t_ao_list *aolist, t_s_env *e)
 		|| (aolist->mode == OR_IF_VALUE && e->ret)
 		|| (aolist->mode == AND_IF_VALUE && !e->ret))
 		if (prepare_command(buff, aolist->cmd, e)
-			|| parse_command(buff, aolist->cmd, e, 1))
+			|| parse_command(aolist->cmd, e))
 			return (1);
 	return (execute_ao_list(buff, aolist->next, e));
 }
