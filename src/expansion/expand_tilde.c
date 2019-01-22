777c51197fef9b290e517c4df3b8e6b9235b223e
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-22 08:11:34 +0100

    [EXPANSION] Fixed leaks on tilde

--- src/expansion/expand_tilde.c
+++ src/expansion/expand_tilde.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 02:35:19 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/05 15:43:57 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/22 04:45:14 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,16 @@ int				expand_tilde(t_exp *param, t_ret *ret)
 	while (i < sizeof(tilde) / sizeof(tilde[0]))
 	{
 		if (tilde[i].comparaison(parameter.word))
-			return (tilde[i].handler(ret, parameter.word, param));
+		{
+			error = tilde[i].handler(ret, parameter.word, param);
+			break ;
+		}
 		i++;
 	}
-	return (expand_tilde_not(ret, parameter.word));
+	if (i == sizeof(tilde) / sizeof(tilde[0]))
+		error = expand_tilde_not(ret, parameter.word);
+	expand_free_t_ret(&parameter, 0);
+	return (error);
 }
 
 int				expand_tilde_not(t_ret *ret, const char *word)
