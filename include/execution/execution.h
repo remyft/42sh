3475054ab4718b1dd9abaf06bce8c1395c0895d6
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 08:41:31 +0100

    [EXECUTION] add redirections in execution

--- include/execution/execution.h
+++ include/execution/execution.h
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 02:17:56 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/22 07:46:34 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 03:10:57 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,18 @@
 
 typedef struct	s_execute
 {
-	t_argument	*variable;
-	t_argument	*command;
-	int			piped;
+	t_argument		*variable;
+	t_argument		*command;
+	t_redirection	*redirection;
+	int				piped;
 }				t_execute;
 
-int				execute_list(const char *buff, t_m_list *list, t_s_env *e);
+int				execute_list(t_m_list *list, t_s_env *e);
 void			quote_removal(t_argument *arg);
 void			variable_assignment(t_command *cmd, t_s_env *e);
 int				parse_command(void *cmd, t_s_env *e);
 int				fork_command(t_execute *exec, t_s_env *e);
-int				execute_command(t_execute *exec, t_s_env *e);
+void			execute_command(t_execute *exec, t_s_env *e);
 char			**duplicate_environ(char **table);
 char			**modify_environ(t_argument *var, t_argument *end, char **env);
 char			**group_command(t_argument *cmd);
