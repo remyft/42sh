1a2b63b8a0490addbde16e5db29bea813a3fda42
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 08:59:10 +0100

    [TOKEN] try to handle comment (#) UNFINISHED

--- src/token/handle_comment.c
+++ src/token/handle_comment.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 00:22:05 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/10 23:46:56 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 08:22:58 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 t_token			*handle_comment(t_param *param, t_call *tokens)
 {
 	(void)tokens;
-	param->token->tail = param->i;
-	param->token->type = TOKEN;
-	if (!param->token->id)
+	if (param->token->type == UNDEFINED)
+	{
+		param->token->type = TOKEN;
 		param->token->id = COMMENT;
+	}
 	return (param->token);
 }
