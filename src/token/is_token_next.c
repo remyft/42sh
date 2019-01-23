3df142d91034ef6b5f907083b1abed0c6ca98deb
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 09:00:29 +0100

    [TOKEN] "-" functions

--- src/token/is_token_next.c
+++ src/token/is_token_next.c
@@ -6,29 +6,11 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 10:22:43 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/22 06:14:46 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 05:59:00 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "token.h"
-
-int			ft_isvalidname(t_param *param)
-{
-	const char	*str;
-	size_t		n;
-
-	str = param->buff + param->token->head;
-	n = param->i - param->token->head;
-	if (!n || ft_isdigit(*str))
-		return (0);
-	while (n--)
-		if (!ft_isalnum(*str) && *str != '_')
-			return (0);
-		else
-			str++;
-	return (1);
-}
 
 int			ft_isword(int c)
 {
@@ -50,4 +32,9 @@ int			ft_iscommand(int c)
 {
 	return ((c == '{') || (c == '(')
 		|| (c == '}') || (c == ')'));
+}
+
+int			ft_isminus(int c)
+{
+	return ((c == '-'));
 }
