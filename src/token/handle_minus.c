2613d26d90d0dbad1cd1fdc0e85bab012cef1378
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 09:00:46 +0100

    [TOKEN] "-" functions

---
+++ src/token/handle_minus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_minus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/23 05:48:00 by gbourgeo          #+#    #+#             */
+/*   Updated: 2019/01/23 06:08:14 by gbourgeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "token.h"
+
+t_token				*handle_minus(t_param *param, t_call *token)
+{
+	(void)token;
+	if (param->token->type == UNDEFINED)
+	{
+		param->token->type = TOKEN;
+		return (param->token);
+	}
+	else if (param->token->type == TOKEN)
+		return (param->token);
+	param->token = token[param->token->type].identifier(param);
+	if (param->token->prev->head[param->token->prev->len - 1] != '&')
+		return (param->token);
+	param->token->next = new_token(param->buff, param->i + 1);
+	param->token->next->prev = param->token;
+	param->token->len++;
+	return (param->token->next);
+}
