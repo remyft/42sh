cae20f5c5722a1c29a99f7baed29efb98100cf8b
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 09:01:14 +0100

    [TOKEN] changes due to new token structure

--- src/token/tdebug.c
+++ src/token/tdebug.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 19:13:49 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/10 23:49:01 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 02:33:48 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 
 #ifndef DEBUG
 
-void			debug_tokens(const char *buff, t_token *tokens)
+void			debug_tokens(t_token *tokens)
 {
-	(void)buff;
 	(void)tokens;
 }
 
 #else
 
-void			debug_tokens(const char *buff, t_token *tokens)
+void			debug_tokens(t_token *tokens)
 {
 	t_token		*ptr;
 
@@ -31,10 +30,8 @@ void			debug_tokens(const char *buff, t_token *tokens)
 	while (ptr)
 	{
 		printf("------------------------------\n"
-				"type:%d id:%d head:%ld tail:%ld\n",
-				ptr->type, ptr->id, ptr->head, ptr->tail);
-		printf("buff   : [%.*s]\n", (int)(ptr->tail - ptr->head),
-									buff + ptr->head);
+				"type:%d id:%d\n", ptr->type, ptr->id);
+		printf("buff   : [%.*s]\n", (int)ptr->len, ptr->head);
 		ptr = ptr->next;
 	}
 }
