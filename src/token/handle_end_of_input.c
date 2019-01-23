b46455139de1362d3769cc00c9e13075220e90dd
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 08:59:30 +0100

    [TOKEN] changes due to new token structure

--- src/token/handle_end_of_input.c
+++ src/token/handle_end_of_input.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 23:42:06 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/10 23:47:08 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 08:21:00 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_token			*handle_end_of_input(t_param *param, t_call *token)
 {
 	t_token		*ret;
 
-	param->token->tail = param->i;
+	param->token->len = (param->buff + param->i) - param->token->head;
 	if (param->token->type != UNDEFINED && param->token->id != NEWLINE
-		&& param->token->head < param->token->tail)
+		&& param->token->id != COMMENT && param->token->len > 1)
 		param->token = token[param->token->type].identifier(param);
 	if ((ret = param->token->prev))
 	{
