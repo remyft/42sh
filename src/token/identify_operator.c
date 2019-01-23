60e9dd6094658ffa49e1633f2694c4726d828ed9
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-23 08:59:54 +0100

    [TOKEN] changes due to new token structure

--- src/token/identify_operator.c
+++ src/token/identify_operator.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 00:38:00 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/10 23:47:28 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 03:15:57 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static size_t	max(size_t a, size_t b)
 	return ((a > b) ? a : b);
 }
 
-static size_t	check_operator(t_token *token, const char *buff, size_t len)
+static size_t	check_operator(t_token *token, size_t len)
 {
 	static t_ope	ope[] = {
 		OP_OR_IF, OP_PIPE, OP_PIPE_AND, OP_NOT, OP_AND_IF, OP_BACKGRND, OP_SEMI,
@@ -34,7 +34,7 @@ static size_t	check_operator(t_token *token, const char *buff, size_t len)
 	while (i < sizeof(ope) / sizeof(ope[0]))
 	{
 		size = max(ft_strlen(ope[i].name), len);
-		if (!ft_strncmp(ope[i].name, buff + token->head, size))
+		if (!ft_strncmp(ope[i].name, token->head, size))
 			return (ope[i].value);
 		i++;
 	}
@@ -45,14 +45,12 @@ t_token			*identify_operator(t_param *param)
 {
 	size_t	ret;
 
-	param->token->tail = param->i;
-	ret = param->i - param->token->head + 1;
-	if (check_operator(param->token, param->buff, ret))
+	param->token->len = (param->buff + param->i) - param->token->head;
+	if (check_operator(param->token, param->token->len + 1))
 		return (param->token);
-	ret = param->token->tail - param->token->head;
-	if ((ret = check_operator(param->token, param->buff, ret)))
+	if ((ret = check_operator(param->token, param->token->len)))
 		param->token->id = ret;
-	param->token->next = new_token(param->buff[param->i], param->i);
+	param->token->next = new_token(param->buff, param->i);
 	param->token->next->prev = param->token;
 	return (param->token->next);
 }
