5dc60a26c7589587d090942e64d5c698a5344f29
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-24 07:15:21 +0100

    [TOKEN] more efficient operator identification

--- src/token/identify_operator.c
+++ src/token/identify_operator.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 00:38:00 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/23 03:15:57 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/24 07:05:32 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@ static size_t	check_operator(t_token *token, size_t len)
 	static t_ope	ope[] = {
 		OP_OR_IF, OP_PIPE, OP_PIPE_AND, OP_NOT, OP_AND_IF, OP_BACKGRND, OP_SEMI,
 		OP_DSEMI, OP_LESS, OP_LESSAND, OP_LESS_GREAT, OP_GREAT, OP_GREAT_PIPE,
-		OP_AND_GREAT, OP_GREAT_AND, OP_DGREAT, OP_AND_GREAT, OP_DGREAT_AND,
-		OP_DLESS, OP_TLESS,
+		OP_AND_GREAT, OP_GREAT_AND, OP_DGREAT, OP_AND_DGREAT, OP_DLESS,
 	};
 	size_t			i;
 	size_t			size;
@@ -43,13 +42,8 @@ static size_t	check_operator(t_token *token, size_t len)
 
 t_token			*identify_operator(t_param *param)
 {
-	size_t	ret;
-
 	param->token->len = (param->buff + param->i) - param->token->head;
-	if (check_operator(param->token, param->token->len + 1))
-		return (param->token);
-	if ((ret = check_operator(param->token, param->token->len)))
-		param->token->id = ret;
+	param->token->id = check_operator(param->token, param->token->len);
 	param->token->next = new_token(param->buff, param->i);
 	param->token->next->prev = param->token;
 	return (param->token->next);
