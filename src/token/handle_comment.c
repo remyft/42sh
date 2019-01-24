87130370bdd083c37c951ed319c804167cb19566
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-24 07:13:48 +0100

    [TOKEN] Comments handled

--- src/token/handle_comment.c
+++ src/token/handle_comment.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 00:22:05 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/23 08:22:58 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/23 22:57:02 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_token			*handle_comment(t_param *param, t_call *tokens)
 	(void)tokens;
 	if (param->token->type == UNDEFINED)
 	{
+		param->token->head = param->buff + param->i;
 		param->token->type = TOKEN;
 		param->token->id = COMMENT;
 	}
