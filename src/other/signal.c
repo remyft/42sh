35c40835ee89f645806e91670f122cbdc8efa63e
Remy FONTAINE <rfontain@e2r7p4.42.fr>
2019-01-24 04:02:39 +0100

    [COMPLETION] Add redirect to deal with completion and winching

--- src/other/signal.c
+++ src/other/signal.c
@@ -6,11 +6,12 @@
 /*   By: rfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 04:50:33 by rfontain          #+#    #+#             */
-/*   Updated: 2018/11/23 08:53:05 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/01/24 03:55:45 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main_tools.h"
+#include "minishell.h"
 
 void	sig_hdlr(int sig)
 {
@@ -25,6 +26,16 @@ void	sig_winch(int sig)
 	line = get_struct();
 	tgetent(NULL, line->term);
 	line->nb_col = tgetnum("co");
+	line->key = WINCH;
+	if (*line->e_cmpl & COMPLETION)
+	{
+		if (line->is_putb < 2)
+		{
+			*line->e_cmpl &= ~COMPLETION;
+		//	line->is_putb--;
+		}
+		get_complet(line);
+	}
 }
 
 void	deal_winch(t_line *line)
