8bec0c7f4408fd15705eb7bc004067fac30e99d6
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-24 07:14:53 +0100

    [TOKEN] more efficient operator identification

--- src/token/handle_operator.c
+++ src/token/handle_operator.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 20:34:30 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/09 02:23:02 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/24 06:42:43 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ t_token			*handle_operator(t_param *param, t_call *token)
 {
 	if (param->token->type == UNDEFINED)
 		param->token->type = OPERATOR;
-	return (token[param->token->type].identifier(param));
+	else if (param->token->type != OPERATOR)
+		param->token = token[param->token->type].identifier(param);
+	return (param->token);
 }
