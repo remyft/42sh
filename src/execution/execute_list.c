1842c51bcb6c791cfd9b762d58af1236b535698a
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-26 20:55:06 +0100

    [EXECUTION] functions rename

--- src/execution/execute_list.c
+++ src/execution/execute_list.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 02:19:16 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/25 15:12:21 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/01/26 09:36:42 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "redirection.h"
 #include "libft.h"
 
-static int	prepare_redirect(t_redirection *cmd, t_s_env *e)
+static int	redirect_prepare(t_redirection *cmd, t_s_env *e)
 {
 	if (!cmd)
 		return (0);
@@ -29,18 +29,18 @@ static int	prepare_redirect(t_redirection *cmd, t_s_env *e)
 	if (cmd->token)
 		if (redirection(&cmd, e))
 			return (1);
-	return (prepare_redirect(cmd->next, e));
+	return (redirect_prepare(cmd->next, e));
 }
 
-static int	prepare_command(void *cmd, t_s_env *e)
+static int	command_prepare(void *cmd, t_s_env *e)
 {
 	if (!cmd)
 		return (0);
 	if (*(int *)cmd == IS_A_PIPE)
-		return (prepare_command(((t_pipeline *)cmd)->left, e)
-				|| prepare_command(((t_pipeline *)cmd)->right, e));
+		return (command_prepare(((t_pipeline *)cmd)->left, e)
+				|| command_prepare(((t_pipeline *)cmd)->right, e));
 	else if (expand_argument(((t_command *)cmd)->args, e)
-		|| prepare_redirect(((t_command *)cmd)->redir, e))
+		|| redirect_prepare(((t_command *)cmd)->redir, e))
 		return (1);
 	quote_removal(((t_command *)cmd)->args);
 	return (0);
@@ -53,8 +53,8 @@ static int	execute_ao_list(t_ao_list *aolist, t_s_env *e)
 	if (!aolist->mode
 		|| (aolist->mode == OR_IF_VALUE && e->ret)
 		|| (aolist->mode == AND_IF_VALUE && !e->ret))
-		if (prepare_command(aolist->cmd, e)
-			|| parse_command(aolist->cmd, e))
+		if (command_prepare(aolist->cmd, e)
+			|| command_parse(aolist->cmd, e))
 			return (1);
 	return (execute_ao_list(aolist->next, e));
 }
