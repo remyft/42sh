c113c113ec7ecb11361ab18c1e6e100c55bfbae1
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-26 20:52:01 +0100

    [EXECUTION] add execve error

--- include/execution/execution_error.h
+++ include/execution/execution_error.h
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 18:30:43 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/24 08:27:40 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/26 08:02:38 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 enum
 {
 	ERR_OK_VAL = 0,
+	ERR_EXEC_VAL,
 	ERR_MALLOC_VAL,
 	ERR_NOT_FOUND_VAL,
 	ERR_PERM_VAL,
 	ERR_IS_DIRECTORY_VAL,
 };
 
+# define ERR_EXEC_STR			"execution failed"
 # define ERR_MALLOC_STR			"not enought memory"
 # define ERR_NOT_FOUND_STR		"command not found"
 # define ERR_PERM_STR			"Permission denied"
