896128f8a283a87955886a77b20375b556ff2ea7
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-01-27 12:56:17 +0100

    [TOKEN] fix equal token

--- src/token/handle_equal.c
+++ src/token/handle_equal.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/12 12:24:11 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/13 19:42:53 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/01/27 12:53:46 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@
 t_token			*handle_equal(t_param *param, t_call *token)
 {
 	(void)token;
-	if ((!param->token->prev || param->token->prev->id == ASSIGNMENT_WORD)
+	if (param->token->type == UNDEFINED)
+		param->token->type = WORD;
+	else if ((!param->token->prev || param->token->prev->id == ASSIGNMENT_WORD)
 		&& ft_isvalidname(param))
 		param->token->id = ASSIGNMENT_WORD;
 	return (param->token);
