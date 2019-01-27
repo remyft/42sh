af87de4e9d1b6144fcaff404225e0a3ab24858a6
Remy FONTAINE <rfontain@e2r6p17.42.fr>
2019-01-27 17:47:42 +0100

    [COMPLETION] Add escape sequence to the file who have to

--- src/completion/create_tree.c
+++ src/completion/create_tree.c
@@ -6,13 +6,33 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 21:15:52 by rfontain          #+#    #+#             */
-/*   Updated: 2019/01/25 12:56:31 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/01/27 16:42:05 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 #include "put.h"
+#include "shell_lib.h"
 #include <dirent.h>
+
+char		*add_escape(char *str, int nb)
+{
+	char	*esc;
+	int		i;
+	int		j;
+
+	if (!(esc = ft_memalloc(sizeof(char) * (ft_strlen(str) + nb + 1))))
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (str[++i])
+	{
+		if (sh_is_escapable(str[i]))
+			esc[j++] = '\\';
+		esc[j++] = str[i];
+	}
+	return (esc);
+}
 
 t_tree		*create_bin_tree(char **env)
 {
@@ -30,6 +50,8 @@ t_tree		*create_file_tree(char *path, t_tree *tern)
 {
 	struct dirent	*indir;
 	DIR				*dir;
+	int				is_esc;
+	char			*tmp;
 
 	if (!path)
 		return (NULL);
@@ -41,7 +63,16 @@ t_tree		*create_file_tree(char *path, t_tree *tern)
 		tern->value = -1;
 	}
 	while ((indir = readdir(dir)))
-		feed_tree(indir->d_name, indir->d_type, &tern, 0);
+	{
+		if ((is_esc = sh_str_isescape(indir->d_name)))
+		{
+			tmp = add_escape(indir->d_name, is_esc);
+			feed_tree(tmp, indir->d_type, &tern, 0);
+			free(tmp);
+		}
+		else
+			feed_tree(indir->d_name, indir->d_type, &tern, 0);
+	}
 	set_psblty(tern);
 	closedir(dir);
 	return (tern);
