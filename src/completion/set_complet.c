0a42aebfef17767bd7e27528b42f55e813bfaa32
Remy FONTAINE <rfontain@e3r4p4.42.fr>
2019-01-29 23:11:00 +0100

    [COMPLETION] Set or not line->tmp[0]

--- src/completion/set_complet.c
+++ src/completion/set_complet.c
@@ -6,7 +6,7 @@
 /*   By: rfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 17:13:29 by rfontain          #+#    #+#             */
-/*   Updated: 2019/01/27 20:39:18 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/01/29 21:42:56 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,21 @@ static int	deal_set(t_line *line, char *ptr, DIR *dir)
 	return (0);
 }
 
-void		set_complet(t_line *line)
+void		set_complet(t_line *line, int set)
 {
 	DIR		*dir;
 	char	*ptr;
+	int		tmp;
 
 	dir = NULL;
 	delete_down();
+	tmp = line->tmp[0];
 	*(line->e_cmpl) &= ~COMPLETION;
 	line->is_putb = 0;
 	ptr = sh_strrchr(line->curr->buff, ' ');
 	line->tmp[0] = deal_set(line, ptr, dir);
+	if (!set)
+		line->tmp[0] = tmp;
 	ft_bzero(line->curr->buff_tmp, ft_strlen(line->curr->buff_tmp));
 	line->curr->buff_tmp[8193] = 0;
 	line->index = line->len;
