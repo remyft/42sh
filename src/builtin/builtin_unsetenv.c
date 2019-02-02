8e8cee3aec98cc1166bd7d01cd3d227c48be7412
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-02-02 20:13:36 +0100

    [BUILTIN] unsetenv rework complete

--- src/builtin/builtin_unsetenv.c
+++ src/builtin/builtin_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/20 23:26:00 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/27 14:16:54 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/02/01 23:02:08 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@
 
 int				builtin_unsetenv(t_execute *exec, t_s_env *e)
 {
-	char		**var;
 	size_t		i;
-	size_t		j;
 
 	i = 1;
 	if (!exec->cmd[i])
@@ -29,17 +27,8 @@ int				builtin_unsetenv(t_execute *exec, t_s_env *e)
 	}
 	while (exec->cmd[i])
 	{
-		if ((var = sh_getnenvaddr(exec->cmd[i], e->public_env)))
-		{
-			free(*var);
-			j = 0;
-			while (var[j + 1])
-			{
-				var[j] = var[j + 1];
-				j++;
-			}
-			var[j] = NULL;
-		}
+		sh_unsetenv(exec->cmd[i], e->public_env);
+		sh_unsetenv(exec->cmd[i], e->private_env);
 		i++;
 	}
 	return (0);
