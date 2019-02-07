14041d3e3899fbe6d8897f6291f67c0b89528890
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-02-07 03:12:42 +0100

    [TOKEN] add function to check if an operator is delimited

--- src/token/handle_newline.c
+++ src/token/handle_newline.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 07:55:06 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/17 03:19:59 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/02/06 02:32:55 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ t_token			*handle_newline(t_param *param, t_call *token)
 		param->token = token[param->token->type].identifier(param);
 	param->token->type = TOKEN;
 	param->token->id = NEWLINE;
+	(void)param;
+	(void)token;
 	return (param->token);
 }
