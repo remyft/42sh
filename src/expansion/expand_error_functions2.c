6136ec1a5b79ba22e618706989beea615dbd3dc3
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-02-15 06:43:04 +0100

    [EXPANSION] expansions error handling method change

--- src/expansion/expand_error_functions2.c
+++ src/expansion/expand_error_functions2.c
@@ -6,60 +6,40 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 22:09:45 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/16 00:52:30 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/02/15 05:04:29 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <unistd.h>
+#include "ft_dprintf.h"
 #include "expansion.h"
 #include "expansion_errors.h"
 
 void			err_no_env_func(const char *progname, t_ret *ret)
 {
 	(void)ret;
-	ft_putstr_fd(progname, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	ft_putstr_fd(ERR_NO_ENV_STR, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
+	ft_dprintf(STDERR_FILENO, "%s: %s\n", progname, ERR_NO_ENV_STR);
 }
 
 void			err_directory_func(const char *progname, t_ret *ret)
 {
 	(void)ret;
-	ft_putstr_fd(progname, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	ft_putstr_fd(ERR_DIRECTORY_STACK_STR, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
+	ft_dprintf(STDERR_FILENO, "%s: %s\n", progname, ERR_DIRECTORY_STACK_STR);
 }
 
 void			err_given_func(const char *progname, t_ret *ret)
 {
-	ft_putstr_fd(progname, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	if (ret)
-		ft_putstr_fd(ret->word, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
+	ft_dprintf(STDERR_FILENO, "%s: %s\n", progname, ret ? ret->word : "");
 }
 
 void			err_no_user_func(const char *progname, t_ret *ret)
 {
-	ft_putstr_fd(progname, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	ft_putstr_fd(ERR_NO_SUCH_USER_STR, STDERR_FILENO);
-	if (ret)
-		ft_putstr_fd(ret->word, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
+	ft_dprintf(STDERR_FILENO, "%s: %s %s\n", progname, ERR_NO_SUCH_USER_STR,
+		ret ? ret->word : "");
 }
 
 void			err_ambigous_func(const char *progname, t_ret *ret)
 {
-	ft_putstr_fd(progname, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	if (ret)
-	{
-		ft_putstr_fd(ret->substitute, STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		ft_putstr_fd(ERR_AMBIGUOUS_STR, STDERR_FILENO);
-	}
-	ft_putchar_fd('\n', STDERR_FILENO);
+	ft_dprintf(STDERR_FILENO, "%s: %s: %s\n", progname,
+		ret ? ret->substitute : "", ERR_AMBIGUOUS_STR);
 }
--- src/expansion/expand_glob.c
+++ src/expansion/expand_glob.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 03:52:43 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/03 17:48:10 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/02/15 05:14:30 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int				expand_glob(t_exp *param, t_ret *ret)
 {
 	(void)param;
 	(void)ret;
+	return (ERR_UNHANDLED);
 	return (ERR_NONE);
 }
