fe3867bd46b1fb99515159af0d90268fee8150d7
Remy FONTAINE <rfontain@e2r3p14.42.fr>
2019-02-21 12:52:41 +0100

    [COMPLETION] Deal on ctrl+C and len == nb_col

--- src/completion/deal_completion.c
+++ src/completion/deal_completion.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 01:38:48 by rfontain          #+#    #+#             */
-/*   Updated: 2019/02/20 19:46:18 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/02/21 12:47:02 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include "put.h"
 #include "shell_lib.h"
 #include "shell_term.h"
-
-#define SLEEPING 300000
 
 static void	put_cpl_screen(t_line *line, int nb_ret)
 {
@@ -36,33 +34,9 @@ static void	put_cpl_screen(t_line *line, int nb_ret)
 	}
 }
 
-#include "stdio.h"
-
-void		deal_complet(t_tree *file, t_line *line)
+int			deal_ret(t_tree *file, t_line *line, int *nb_ret, int put)
 {
-	int		put;
-	int		nb_ret;
-	int		len;
-	int		todo;
-
-	put = 0;
-	nb_ret = 0;
-	todo = 0;
-	len = line->len + line->lprompt;
-	if (!line->curr->buff_tmp[8193])
-	{
-		ft_strcpy(line->curr->buff_tmp, line->curr->buff);
-		line->curr->buff_tmp[8193] = 1;
-		todo = 1;
-	}
-	else
-		put = 1;
-	go_home(line);
-	tputs(tgetstr("cd", NULL), 1, ft_pchar);
-	go_end(line);
-	tputs(tgetstr("do", NULL), 1, ft_pchar);
-	tputs(tgoto(tgetstr("ch", NULL), 0, 0), 1, ft_pchar);
-	if ((put = put_complet(file, &put, line, &nb_ret)) == 1)
+	if ((put = put_complet(file, &put, line, nb_ret)) == 1)
 	{
 		*line->e_cmpl |= COMPLETION;
 		line->tmp[0] = 10;
@@ -81,11 +55,37 @@ void		deal_complet(t_tree *file, t_line *line)
 			tputs(tgetstr("do", NULL), 1, ft_pchar);
 			tputs(tgetstr("cr", NULL), 1, ft_pchar);
 		}
-		return ;
+		return (1);
 	}
-	if ((ft_strlen(line->curr->buff_tmp) + line->lprompt) > line->nb_col)
+	return (0);
+}
+
+void		deal_complet(t_tree *file, t_line *line)
+{
+	int		nb_ret;
+
+	nb_ret = 0;
+	go_home(line);
+	tputs(tgetstr("cd", NULL), 1, ft_pchar);
+	go_end(line);
+	tputs(tgetstr("do", NULL), 1, ft_pchar);
+	tputs(tgoto(tgetstr("ch", NULL), 0, 0), 1, ft_pchar);
+	if (!line->curr->buff_tmp[8193])
+	{
+		ft_strcpy(line->curr->buff_tmp, line->curr->buff);
+		line->curr->buff_tmp[8193] = 1;
+		if (deal_ret(file, line, &nb_ret, 0))
+			return ;
+	}
+	else
+	{
+		if (deal_ret(file, line, &nb_ret, 1))
+			return ;
+	}
+	if ((ft_strlen(line->curr->buff_tmp) + line->lprompt) >= line->nb_col)
 		tputs(tgetstr("up", NULL), 1, ft_pchar);
-	if ((line->len + line->lprompt) % line->nb_col < (ft_strlen(line->curr->buff_tmp) + line->lprompt) % line->nb_col)
+	if ((line->len + line->lprompt) % line->nb_col <
+			(ft_strlen(line->curr->buff_tmp) + line->lprompt) % line->nb_col)
 	{
 		if (line->is_putb >= 1)
 			tputs(tgetstr("up", NULL), 1, ft_pchar);
