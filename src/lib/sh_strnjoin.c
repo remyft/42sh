01734881e1ca290b0488f8eba7328ca9feffa11b
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-02-28 00:34:07 +0100

    [LIB] strnjoin

---
+++ src/lib/sh_strnjoin.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_strnjoin.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 23:35:18 by gbourgeo          #+#    #+#             */
+/*   Updated: 2019/02/25 23:38:30 by gbourgeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char			*sh_strnjoin(const char *s1, const char *s2, size_t len)
+{
+	char		*ret;
+
+	if (!(ret = ft_memalloc(len + ft_strlen(s2) + 1)))
+		return (NULL);
+	ret = ft_strncpy(ret, s1, len);
+	ret = ft_strcat(ret, s2);
+	return (ret);
+}
