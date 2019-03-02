9a03d3e8ea3611c403b95b596ea0aef11a444941
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-02 16:04:08 +0100

    [EXPANSION] expand subshells

--- src/expansion/expand_end.c
+++ src/expansion/expand_end.c
@@ -6,14 +6,14 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 22:29:26 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/31 19:23:28 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/02 15:37:30 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "expansion.h"
 #include "expansion_errors.h"
-
+	#include <stdio.h>
 int				expand_end(t_ret *ret, t_argument *arg)
 {
 	t_ret		*ptr;
@@ -29,6 +29,7 @@ int				expand_end(t_ret *ret, t_argument *arg)
 	len = 0;
 	while (ptr)
 	{
+		printf("ptr: %s\n", ptr->word);
 		arg->cmd[len++] = ptr->word;
 		ptr->word = NULL;
 		ptr = ptr->next;
