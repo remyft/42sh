f68de53a1fa431a4093c4f6eaa57a6165faaabab
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-03 18:28:12 +0100

    [EXPANSION] glob

--- src/expansion/expand_glob.c
+++ src/expansion/expand_glob.c
@@ -6,18 +6,28 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 03:52:43 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/03 18:06:38 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/03 18:27:58 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expansion.h"
 #include "expansion_errors.h"
 	#include <stdio.h>
+#include "struct.h"
+#include "globing.h"
+
 int				expand_glob(t_exp *param, t_ret *ret)
 {
+	char	*ptr;
+	t_line	*line;
+	t_slst	*glob;
+
 	(void)param;
 	(void)ret;
 	printf("glob: [%.*s]\n", (int)param->buff_len, param->buff + param->i);
+	ptr = ft_strndup(param->buff + param->i, (int)param->buff_len);
+	line = get_struct();
+	glob = deal_globing(ptr, line->tree[1]);
 	param_addchar(param->buff[param->i], ret);
 	return (ERR_UNHANDLED);
 	return (ERR_NONE);
