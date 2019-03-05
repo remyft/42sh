13be4c88fc98651c794aeee312315941d2e2b021
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-05 18:19:32 +0100

    [COMMAND] add good error string value on file access

--- src/command/command_access.c
+++ src/command/command_access.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 19:49:32 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/03 20:59:56 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/03 21:11:00 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int				command_access(char *path, char *absolute_path)
 		return (ERR_MALLOC);
 	if (access(path, F_OK))
 		return ((ft_strchr(absolute_path, '/')) ?
-			ERR_NO_SUCH_FILE : ERR_NOT_FOUND);
+		ERR_NO_SUCH_FILE : ERR_NOT_FOUND);
 	if (access(path, X_OK))
 		return (ERR_PERM);
 	if (stat(path, &buf))
