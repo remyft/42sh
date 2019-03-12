0cbc7003cb28559c2855f8122e83ee1db87a9b27
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-12 14:09:10 +0100

    [COMMAND] update "_" env variable on command execution

--- src/command/command_check.c
+++ src/command/command_check.c
@@ -6,13 +6,12 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:02:57 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/05 19:50:06 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/11 17:57:51 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "shell_lib.h"
-#include "command.h"
 #include "builtins.h"
 
 int				command_check(t_execute *exec, t_s_env *e)
@@ -22,18 +21,23 @@ int				command_check(t_execute *exec, t_s_env *e)
 		BUILTIN_SETENV, BUILTIN_UNALIAS, BUILTIN_UNSETENV,
 	};
 	size_t				i;
-	char				*last;
+	int					ret;
 
 	i = 0;
-	if (!exec->cmd || !exec->cmd[0])
-		return (0);
-	last = exec->cmd[sh_tablen((const char **)exec->cmd) - 1];
-	sh_setenv("_", last, &e->public_env);
-	while (i < sizeof(builtins) / sizeof(builtins[0]))
+	ret = 0;
+	if (exec->cmd && exec->cmd[0])
 	{
-		if (!ft_strcmp(builtins[i].name, exec->cmd[0]))
-			return (command_builtin(builtins[i].handler, exec, e));
-		i++;
+		if ((ret = sh_tablen((const char **)exec->cmd)))
+			ret--;
+		sh_setenv("_", exec->cmd[ret], &e->public_env);
+		while (i < sizeof(builtins) / sizeof(builtins[0]))
+		{
+			if (!ft_strcmp(builtins[i].name, exec->cmd[0]))
+				return (command_builtin(builtins[i].handler, exec, e));
+			i++;
+		}
+		ret = command_system(exec, e);
 	}
-	return (command_system(exec, e));
+	command_free(exec, NULL);
+	return (ret);
 }
