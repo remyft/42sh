a1c8604147c4b68e4330634a065683e3f5f1428e
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-18 20:54:24 +0100

    [REDIRECTION] fix bug on redirection argument

--- src/redirection/redirection.c
+++ src/redirection/redirection.c
@@ -6,11 +6,13 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 00:59:23 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/01/24 07:31:32 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/18 20:30:14 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "redirection.h"
+#include "redirection_errors.h"
 #include "operator_types.h"
 
 int				redirection(t_redirection **redir, t_s_env *e)
@@ -20,8 +22,17 @@ int				redirection(t_redirection **redir, t_s_env *e)
 		redirect_great_pipe, redirect_and_great, redirect_great_and,
 		redirect_dgreat, redirect_and_dgreat, redirect_dless,
 	};
+	char			*token;
 
 	(*redir)->fdio = -1;
 	(*redir)->fdarg = -1;
+	if (!(*redir)->arg->cmd || !(*redir)->arg->cmd[0])
+	{
+		token = ft_strndup((char *)(*redir)->arg->token->head,
+						(*redir)->arg->token->len);
+		redirect_error(ERR_AMBIGUOUS, token, e);
+		free(token);
+		return (1);
+	}
 	return (handler[(*redir)->token->id - LESS_VALUE](redir, e));
 }
