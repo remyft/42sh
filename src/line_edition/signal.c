ce509ac733400c9e67bf272178955e1fd20d70bc
Remy FONTAINE <rfontain@e3r4p5.42.fr>
2019-03-23 17:16:47 +0100

    [WINCH] Added resize handling on winch

--- src/line_edition/signal.c
+++ src/line_edition/signal.c
@@ -6,19 +6,43 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 04:50:33 by rfontain          #+#    #+#             */
-/*   Updated: 2019/03/18 18:08:32 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/03/23 16:53:40 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main_tools.h"
 #include "put.h"
 
-void	sig_hdlr(int sig)
+void		sig_hdlr(int sig)
 {
 	(void)sig;
 }
 
-void	sig_winch(int sig)
+static void	deal_put_winch(t_line *line)
+{
+	size_t	index;
+
+	index = line->index;
+	go_home(line);
+	tputs(tgetstr("cr", NULL), 1, ft_pchar);
+	tputs(tgetstr("cd", NULL), 1, ft_pchar);
+	put_prompt(line->prompt);
+	ft_putstr(line->curr->buff);
+	line->index = line->len;
+	while (line->index != index)
+	{
+		left_arrow(line);
+		line->index--;
+	}
+	if (*line->e_cmpl & COMPLETION)
+	{
+		if (line->is_putb < 2)
+			*line->e_cmpl &= ~COMPLETION;
+		get_complet(line);
+	}
+}
+
+void		sig_winch(int sig)
 {
 	t_line *line;
 
@@ -28,10 +52,5 @@ void	sig_winch(int sig)
 	line->nb_col = tgetnum("co");
 	line->nb_line = tgetnum("li");
 	line->key = WINCH;
-	if (*line->e_cmpl & COMPLETION)
-	{
-		if (line->is_putb < 2)
-			*line->e_cmpl &= ~COMPLETION;
-		get_complet(line);
-	}
+	deal_put_winch(line);
 }
