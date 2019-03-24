3b91bb561a995ed02ffd508157dc2724b2dda17a
Remy FONTAINE <rfontain@e1r12p4.42.fr>
2019-03-24 20:55:37 +0100

    [LINE_EDITION] Added line return if the cursor isn't on the first position + norme

--- include/shell.h
+++ include/shell.h
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/28 20:49:03 by rfontain          #+#    #+#             */
-/*   Updated: 2019/03/23 19:14:36 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/24 20:16:45 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,10 @@ char			**collect_env(char **ep);
 
 void			launch_rc(t_s_env *e, int fd, char *path);
 void			get_rc(t_s_env *e);
+
+void			launch_new_smd(char **line, t_s_env *e);
+void			init_shell_line(t_line **line, t_s_env *e);
+void			shell_loop(t_line *line, t_s_env *e);
 
 /*
 **	Select, copy, cut and paste
