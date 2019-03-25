d365be035f7bf4ee720a4522ba0f4f740b586ae9
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-03-25 15:53:30 +0100

    [PARSER] fix bug on list with no argument

--- src/parser/parse_list.c
+++ src/parser/parse_list.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 20:39:58 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/12 18:41:29 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/03/24 17:01:19 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 
 int				parse_list(t_token **token, t_p_param *param, t_s_env *e)
 {
+	t_command	*cmd;
+
+	cmd = (t_command *)(*param->list)->aolist->cmd;
+	if (!cmd->args)
+		return (parse_error(ERR_UNEXPECTED_TOKEN, *token, e));
 	if ((*token)->next && !new_tree(*token, param, &(*param->list)->next))
 		return (parse_error(ERR_MALLOC_FAILED, NULLTOKEN, e));
 	return (1);
