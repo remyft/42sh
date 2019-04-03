e99fe65dfd1f12f2645799a842114a54d5548767
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-04-03 18:30:50 +0200

    [COMMAND] fix bug on redirection with no command and term restore

--- src/command/command_parse.c
+++ src/command/command_parse.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 01:23:07 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/26 12:50:02 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/04/03 17:32:24 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int				command_parse(void *cmd, t_s_env *e)
 	int			ret;
 
 	ft_memset(&exec, 0, sizeof(exec));
+	ret = 0;
 	if (!e->forked)
 		term_restore(&e->save);
 	if (*(int *)cmd == IS_A_PIPE)
@@ -28,11 +29,12 @@ int				command_parse(void *cmd, t_s_env *e)
 	else
 	{
 		command_debug(cmd);
-		if (!((t_command *)cmd)->args)
-			return (0);
-		exec.variable = ((t_command *)cmd)->args;
-		exec.redirection = ((t_command *)cmd)->redir;
-		ret = command_prepare(&exec, e);
+		if (((t_command *)cmd)->args)
+		{
+			exec.variable = ((t_command *)cmd)->args;
+			exec.redirection = ((t_command *)cmd)->redir;
+			ret = command_prepare(&exec, e);
+		}
 	}
 	if (!e->forked)
 		define_new_term(&e->save);
