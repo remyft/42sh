d351580c208e3444a3d8e3d321515a259e91b5e0
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-04-06 21:56:17 +0200

    [BUILTIN] echo -e

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -6,15 +6,12 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/25 06:26:16 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/03/24 20:57:40 by rfontain         ###   ########.fr       */
+/*   Updated: 2019/04/06 21:54:37 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "ft_dprintf.h"
-#include "command.h"
-#include "shell_env.h"
-#include "shell_term.h"
+#include "builtin_echo.h"
 #include "builtins.h"
 
 static int		echo_error(t_s_env *e)
@@ -24,35 +21,63 @@ static int		echo_error(t_s_env *e)
 	return (1);
 }
 
-static int		builtin_write_end(int n, t_s_env *e)
+static int		builtin_echo_write(char *arg, int opt)
 {
-	if (!n && ft_dprintf(STDOUT_FILENO, "\n") < 0)
-		return (echo_error(e));
+	size_t		i;
+	char		c;
+
+	i = 0;
+	if (opt & ECHO_OPT_E)
+	{
+		while (arg[i])
+		{
+			if (arg[i] == '\\' && arg[i + 1] == 'n' && ++i)
+				c = '\n';
+			else
+				c = arg[i];
+			if (ft_dprintf(STDOUT_FILENO, "%c", c) < 0)
+				return (1);
+			i++;
+		}
+	}
+	else if (ft_dprintf(STDOUT_FILENO, "%s", arg) < 0)
+		return (1);
+	return (0);
+}
+
+static int		builtin_echo_option(char *arg, int *opt)
+{
+	int			i;
+
+	i = 0;
+	while (arg[++i])
+		if (arg[i] == 'n')
+			*opt |= ECHO_OPT_N;
+		else if (arg[i] == 'e')
+			*opt |= ECHO_OPT_E;
+		else
+			return (1);
 	return (0);
 }
 
 int				builtin_echo(t_execute *exec, t_s_env *e)
 {
 	int			i;
-	int			j;
-	int			n;
+	int			opt;
 
 	i = 1;
-	n = 0;
+	opt = 0;
 	while (exec->cmd[i] && exec->cmd[i][0] == '-' && exec->cmd[i][1])
-	{
-		j = 1;
-		if ((n = (exec->cmd[i][j] == 'n')))
-			while (exec->cmd[i][j] == 'n')
-				j++;
-		if (exec->cmd[i][j] != '\0' && ((i > 1) || !(n = 0)))
+		if (builtin_echo_option(exec->cmd[i], &opt))
 			break ;
-		i++;
-	}
+		else
+			i++;
 	while (exec->cmd[i])
-		if (ft_dprintf(STDOUT_FILENO, "%s", exec->cmd[i]) < 0)
+		if (builtin_echo_write(exec->cmd[i], opt))
 			return (echo_error(e));
-		else if (exec->cmd[++i] && ft_dprintf(STDOUT_FILENO, " ") < 0)
-			return (echo_error(e));
-	return (builtin_write_end(n, e));
+		else
+			i++;
+	if (!(opt & ECHO_OPT_N) && ft_dprintf(STDOUT_FILENO, "\n") < 0)
+		return (echo_error(e));
+	return (0);
 }
