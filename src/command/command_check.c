a86851f3d6aff52f6d2bad4d344de233a26c1d1b
gilles BOURGEOIS <gbourgeo@student.42.fr>
2019-04-13 16:26:37 +0200

    [COMMAND] norme

--- src/command/command_check.c
+++ src/command/command_check.c
@@ -6,13 +6,26 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:02:57 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/04/12 16:41:09 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/04/12 17:13:29 by gbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "shell_lib.h"
 #include "builtins.h"
+
+static int		command_check_builtin(int (*handler)(t_execute *, t_s_env *),
+t_execute *exec, t_s_env *e)
+{
+	int			ret;
+	int			len;
+
+	ret = command_builtin(handler, exec, e);
+	if ((len = sh_tablen((const char **)exec->cmd)))
+		len--;
+	sh_setenv("_", exec->cmd[len], &e->public_env);
+	return (ret);
+}
 
 int				command_check(t_execute *exec, t_s_env *e)
 {
@@ -26,22 +39,18 @@ int				command_check(t_execute *exec, t_s_env *e)
 
 	i = 0;
 	ret = 0;
-	if ((len = sh_tablen((const char **)exec->cmd)))
-		len--;
-	sh_setenv("_", exec->cmd[0], &e->public_env);
 	if (exec->cmd && exec->cmd[0])
 	{
+		sh_setenv("_", exec->cmd[0], &e->public_env);
 		while (i < sizeof(builtins) / sizeof(builtins[0]))
 		{
 			if (!ft_strcmp(builtins[i].name, exec->cmd[0]))
-			{
-				ret = command_builtin(builtins[i].handler, exec, e);
-				sh_setenv("_", exec->cmd[len], &e->public_env);
-				return (ret);
-			}
+				return (command_check_builtin(builtins[i].handler, exec, e));
 			i++;
 		}
 		ret = command_system(exec, e);
+		if ((len = sh_tablen((const char **)exec->cmd)))
+			len--;
 		sh_setenv("_", exec->cmd[len], &e->public_env);
 	}
 	return (ret);
