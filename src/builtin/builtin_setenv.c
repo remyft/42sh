a0aecf01d13ffcc5c0e0d8a77cbce6199ba00920
Remy FONTAINE <rfontain@e2r8p3.42.fr>
2019-04-23 07:00:13 +0200

    [COMPLETION] Added path checking to setenv and unsetenv

--- src/builtin/builtin_setenv.c
+++ src/builtin/builtin_setenv.c
@@ -6,7 +6,7 @@
 /*   By: gbourgeo <gbourgeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 09:37:46 by gbourgeo          #+#    #+#             */
-/*   Updated: 2019/04/16 20:18:19 by gbourgeo         ###   ########.fr       */
+/*   Updated: 2019/04/23 01:59:22 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "ft_dprintf.h"
 #include "shell_lib.h"
 #include "builtin_setenv.h"
+#include "main_tools.h"
 
 static int		setenv_error(int err, char *cmd_name, t_s_env *e)
 {
@@ -51,5 +52,7 @@ int				builtin_setenv(t_execute *exec, t_s_env *e)
 		sh_unsetenv(exec->cmd[1], e->private_env);
 	if (sh_setenv(exec->cmd[1], exec->cmd[2], &e->public_env))
 		return (setenv_error(ERR_MALLOC_ERROR, exec->cmd[0], e));
+	if (ft_strcmp(exec->cmd[1], "PATH") == 0)
+		check_path(e->public_env);
 	return (0);
 }
