ed6c4ebdf4af35a605ba8c8a3fc787f1eec8e4f8
Tsitsi SADAGHASHVILI <tsisadag@e2r3p16.42.fr>
2019-04-24 17:48:28 +0200

    set: printing of \t \n fixed

--- include/builtin/builtin_set.h
+++ include/builtin/builtin_set.h
@@ -6,12 +6,14 @@
 /*   By: tsisadag <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 20:13:46 by tsisadag          #+#    #+#             */
-/*   Updated: 2019/03/09 20:30:36 by tsisadag         ###   ########.fr       */
+/*   Updated: 2019/04/24 17:43:32 by tsisadag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_SET_H
 # define BUILTIN_SET_H
 # include "ft_printf.h"
+
+void	print_set(char *s);
 
 #endif
